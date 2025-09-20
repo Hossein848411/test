19d0d94269223f6b686e328a71596603b9518f30
Hossein848411 <hossin8484mahdave@gmail.com>
2025-09-21 03:07:32 +0330

    Use psycopg2-binary and set runtime python-3.11 for Vercel

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,4 @@ pip3 install -r requirements.txt
 python3 manage.py createsuperuser
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3.11 manage.py collectstatic
+python3 manage.py collectstatic
