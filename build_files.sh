258bb4ebd07b284be2001ff3b1f7aeaf0a6413a4
Hossein848411 <hossin8484mahdave@gmail.com>
2025-09-21 00:57:58 +0330

    Fiest

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,4 @@ pip3 install -r requirements.txt
 python3 manage.py createsuperuser
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3.14 manage.py collectstatic
+python3.12 manage.py collectstatic
