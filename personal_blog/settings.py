f6ee9a83bc2ce32fc399dcdebb1a86354e1f1fd8
Hossein848411 <hossin8484mahdave@gmail.com>
2025-09-21 03:47:24 +0330

    Secure build: install deps, collectstatic; skip DB migrations during build5

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static/"]
 
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build',"static/") 
 
 MEDIA_URL = "/media/"
